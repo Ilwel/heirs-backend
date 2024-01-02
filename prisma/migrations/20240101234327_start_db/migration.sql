25c5361f123e2ba236850055b30ade131a40e436
iueu <ilwelisaac@hotmail.com>
2024-01-01 21:07:49 -0300

    chore: init project

---
+++ prisma/migrations/20240101234327_start_db/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_id_key" ON "users"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
