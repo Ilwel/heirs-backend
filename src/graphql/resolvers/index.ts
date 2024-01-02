9b4360717fff58166c2f5c042f94bdb9dfb80ec2
ILWEL ISAAC <ilwelisaac@hotmail.com>
2024-01-02 13:32:01 -0300

    feat: decorate typegraphql-prisma auto resolvers

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,5 +1,10 @@
 import { type NonEmptyArray } from 'type-graphql'
-import { BoardCrudResolver, PlayerCrudResolver } from '../../../prisma/generated/type-graphql'
+import { BoardCrudResolver, FindManyUserResolver, PlayerCrudResolver } from '../../../prisma/generated/type-graphql'
+import UserResolver from './user/user.resolver'
+import { Service } from 'typedi'
 
+Service()(PlayerCrudResolver)
+Service()(BoardCrudResolver)
+Service()(FindManyUserResolver)
 // eslint-disable-next-line
-export default [PlayerCrudResolver, BoardCrudResolver] as NonEmptyArray<Function>
+export default [PlayerCrudResolver, BoardCrudResolver, UserResolver, FindManyUserResolver] as NonEmptyArray<Function>
