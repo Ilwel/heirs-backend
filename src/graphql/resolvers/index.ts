400b31ecc40f24e8c106a19ad44495e2fce7135f
iueu <ilwelisaac@hotmail.com>
2024-01-05 20:50:23 -0300

    feat: expouse friendship crud resolver

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,10 +1,11 @@
 import { type NonEmptyArray } from 'type-graphql'
-import { BoardCrudResolver, FindManyUserResolver, PlayerCrudResolver } from '../../../prisma/generated/type-graphql'
+import { BoardCrudResolver, FindManyUserResolver, PlayerCrudResolver, FriendshipCrudResolver } from '../../../prisma/generated/type-graphql'
 import UserResolver from './user/user.resolver'
 import { Service } from 'typedi'
 
 Service()(PlayerCrudResolver)
 Service()(BoardCrudResolver)
 Service()(FindManyUserResolver)
+Service()(FriendshipCrudResolver)
 // eslint-disable-next-line
-export default [PlayerCrudResolver, BoardCrudResolver, UserResolver, FindManyUserResolver] as NonEmptyArray<Function>
+export default [PlayerCrudResolver, BoardCrudResolver, UserResolver, FindManyUserResolver, FriendshipCrudResolver] as NonEmptyArray<Function>
